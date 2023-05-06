c6aba7f9fd277402857ffe3ea8c9585de69e90fd
jeremygradisher <jeremygradisher@gmail.com>
2023-05-06 00:29:38 -0400

    Show and Edit users are working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,47 +1,39 @@
 class UsersController < ApplicationController
-  
-    def new
-      @user   = User.new
-    end
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
   
     def index
       @users = User.all
     end
-    
+  
     def show
       @user = User.find(params[:id])
     end
-    
+  
     def edit
       @user = User.find(params[:id])
     end
-    
+  
     def update
       @user = User.find(params[:id])
-      respond_to do |format|
-        if @user.update(user_params)
-          format.html { redirect_to @user, notice: 'User was successfully updated.' }
-          format.json { render :show, status: :ok, location: @user }
-        else
-          format.html { render :edit }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
-        end
+      if @user.update(user_params)
+        redirect_to @user, notice: 'User was successfully updated.'
+      else
+        render :edit
       end
     end
-  
   
     def destroy
-      @user = User.find(params[:id])
       @user.destroy
-      respond_to do |format|
-        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-        format.json { head :no_content }
-      end
-    end
-    
-    private
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
+      redirect_to users_url, notice: 'User was successfully destroyed.'
     end
   
+    private
+      def set_user
+        @user = User.find(params[:id])
+      end
+  
+      def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :is_admin)
+      end
   end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # lets add some routes for our Users
   #resources :users, :only => [:index, :show, :destroy]
-  resources :users
+  resources :users, except: [:new]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
