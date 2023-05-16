ade8e04125742c5b626551c0971e70a50d35229f
jeremygradisher <jeremygradisher@gmail.com>
2023-05-16 11:41:25 -0400

    Added rspec

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,9 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "username should be required" do
+    user = User.new(email: "example@example.com", password: "password")
+    assert_not user.valid?
+    assert user.errors[:username].any?
+  end
 end
